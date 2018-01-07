89941ba79c7fe0cd1e46fbdaf7685a32c0f87ca0
champseo2017 <boomgt4444@gmail.com>
2018-01-07 18:15:31 +0700

    g

--- src/Main.js
+++ src/Main.js
@@ -30,7 +30,7 @@ export default class Header extends Component {
 
        <footer class="py-5 bg-dark">
          <div class="container">
-           <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+           <p class="m-0 text-center text-white">Copyright &copy; bMJU 2017</p>
          </div>
 
        </footer>
